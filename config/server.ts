92d600004429c81d2e45cc7d2ce864957646076f
thesunnymallick <alfesunnymallick800@gmail.com>
2025-12-07 12:46:20 +0530

    Setup Cloudinary uploads

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,16 @@
-export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+module.exports = ({ env }) => ({
+  host: '0.0.0.0',
   port: env.int('PORT', 1337),
+
+  proxy: true,
+
   app: {
     keys: env.array('APP_KEYS'),
+  },
+
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
   },
 });
